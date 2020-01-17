9062cb4ecc49660b318bc1c47ebd146eb1f4f63a
Mika <mika.morsky@gmail.com>
2020-01-17 17:24:40 +0200

    Second commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 8080;
 
 let app = express();
 
@@ -6,13 +7,21 @@ app.use((req, res, next) => {
     console.log('PATH: ' + req.path);
     next();
 });
-
+app.use('/testi',(req, res, next) => {
+    console.log('moro');
+    next();
+});
 
 app.get('/',(req, res, next) => {
     res.send('Hello');
     //res.end();
 });
 
+app.use((req, res, next) => {
+    console.log('404');
+    res.status(404);
+    res.send(`<html>page not found</html>`);
+})
 
 
-app.listen(8080);
+app.listen(PORT);
